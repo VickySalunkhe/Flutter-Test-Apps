ab8f317e8daa40138427f2f0a49f99b59a22a346
vicky <salunkhevicky01@gmail.com>
2018-06-21 18:47:29 +0530

    Add existing file

--- test3/lib/second.dart
+++ test3/lib/second.dart
@@ -4,6 +4,13 @@ import 'events.dart';
 import 'main.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
+List<String> _getdiamonQuality = [
+  "VVS1", "VVS2", "VS1", "VS2", "S1", "S2", "I1", "I2", "I3", "Single Cut",
+];
+
+List<String> _getkarat = [
+  "9 KT","10 KT","14 KT","18 KT",
+];
 final List<String> productImage = [
   "https://personalproject1.000webhostapp.com/images/product/SR-R-126115..jpg",
   "https://personalproject1.000webhostapp.com/images/product/SR-R-126115..jpg",
@@ -45,16 +52,16 @@ const List<_ProductDetails> _productDetails = const <_ProductDetails>[
 ];
 
 class _CustomizePage {
-  const _CustomizePage({this.text});
-  
+  const _CustomizePage({this.pos, this.text});
+  final int pos;
   final String text;
 }
 
 const List<_CustomizePage> _customizePages = const <_CustomizePage>[
-  const _CustomizePage(text: 'Karat'),
-  const _CustomizePage(text: 'Diamond Quality'),
-  const _CustomizePage(text: 'Colour'),
-  const _CustomizePage(text: 'Standard'),
+  const _CustomizePage(pos: 0, text: 'Karat'),
+  const _CustomizePage(pos: 1, text: 'Diamond Quality'),
+  const _CustomizePage(pos: 2, text: 'Colour'),
+  const _CustomizePage(pos: 3, text: 'Standard'),
 ];
 
 class ProductDetails extends StatefulWidget {
@@ -74,22 +81,7 @@ class ProductDetailsState extends State<ProductDetails>
     _controller = new TabController(
         vsync: this, length: _customizePages.length, initialIndex: 0);
   }
-
-  String _getColour(int index){
-    String name;
-    switch (index) {
-      case 0: name="White Gold";
-        break;
-      case 1: name="Gold";
-        break;
-      case 2: name="Rose Gold";
-        break;
-      case 3: name="Yellow Gold";
-        break;
-      default:
-    }
-    return name;
-  }
+List<String> _getColour = ["White Gold","Gold","Rose Gold","Yellow Gold"];
 
   _menuLayout() {
     return new Container(
@@ -242,6 +234,118 @@ class ProductDetailsState extends State<ProductDetails>
     );
   }
 
+  int _iCount = 0;
+
+  Widget _colour(int _itemCount) {
+    return new GridView.builder(
+      controller: ScrollController(),
+      gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 5,
+        childAspectRatio: 0.65,
+      ),
+      itemBuilder: (BuildContext context, int index) {
+        return new Container(
+            padding: const EdgeInsets.all(10.0),
+            child: new GestureDetector(
+                onTap: () {
+                  print("Colour Tap, $index  -> " + _getColour[index]);
+                },
+                child: new Column(children: [
+                  new Image.network(
+                    "https://personalproject1.000webhostapp.com/images/colour_$index.png",
+                    fit: BoxFit.contain,
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.only(top: 5.0),
+                    child: new Text(_getColour[index],
+                        textAlign: TextAlign.center),
+                  ),
+                ])));
+      },
+      itemCount: _itemCount,
+    );
+  }
+ 
+  Widget _karat(int _itemCount) {
+    return new Container(
+      alignment: Alignment.center,
+      margin: const EdgeInsets.only(top:22.0,left:10.0),
+    child: new GridView.builder(
+      controller: ScrollController(),
+      gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 5,
+     //   childAspectRatio: 1.0,
+      ),
+      itemBuilder: (BuildContext context, int index) {
+        return new GestureDetector(
+          onTap: (){print("Karat Tap $index -> "+_getkarat[index]);
+          },
+          child: new Container(
+            alignment: Alignment.center, 
+            margin: const EdgeInsets.all(5.0),
+            decoration: new BoxDecoration(
+               border: new Border.all(color: Colors.black26)
+            ),
+              child: new Text(_getkarat[index], style: TextStyle(fontSize: 16.0),),
+          ),
+        );
+      },
+      itemCount: _itemCount,
+    ));
+  }
+
+  Widget _diamond(int _itemCount) {
+    return new GridView.builder(
+      controller: ScrollController(),
+      gridDelegate: new SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: MediaQuery.of(context).size.width/5,
+        childAspectRatio: 1.27,
+      ),
+      itemBuilder: (BuildContext context, int index) {
+        return new Container(
+          margin: EdgeInsets.only(top:10.0),
+            //padding: const EdgeInsets.only(top:5.0,left:10.0,right:10.0,bottom: 4.0),
+            child: new GestureDetector(
+                onTap: () {
+                  print("Diamond Tap, $index  -> "+ _getdiamonQuality[index]);
+                },
+                child: new Column(children: [
+                  new Icon(Icons.ac_unit),
+                  new Padding(
+                    padding: const EdgeInsets.only(top: 4.0),
+                    child: new Text(_getdiamonQuality[index],
+                        textAlign: TextAlign.center),
+                  ),
+                ])));
+      },
+      itemCount: _itemCount,
+    );
+  }
+
+  Widget _standard(int _itemCount) {
+    return new Container(
+        padding: const EdgeInsets.all(10.0),
+        child: new GestureDetector(
+            onTap: () {
+              print("standard Tap, $_itemCount  -> ");
+            },
+            child: new Column(children: [
+              new Padding(
+                padding: const EdgeInsets.only(top: 5.0),
+                child:
+                    const Radio<int>(value: 1, groupValue: 0, onChanged: null),
+              ),
+              new Text("GH Standard"),
+            ])));
+  }
+
+  int radioValue = 0;
+  void handleRadioValueChanged(int value) {
+    setState(() {
+      radioValue = value; 
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -323,7 +427,8 @@ class ProductDetailsState extends State<ProductDetails>
           ),
           new SliverToBoxAdapter(
             child: new Column(
-              children: <Widget>[
+             mainAxisSize: MainAxisSize.max,
+           children: <Widget>[ 
                 new Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: new Text(
@@ -334,63 +439,198 @@ class ProductDetailsState extends State<ProductDetails>
                     ),
                   ),
                 ),
+                new Container(
+                  decoration: new BoxDecoration(color: Colors.white10),
+                  child: new TabBar(
+                      labelColor: Colors.black87,
+                      indicatorWeight: 4.0,
+                      indicatorColor: Colors.black,
+                      controller: _controller,
+                      isScrollable: true,
+                      labelStyle: TextStyle(fontSize: 16.0),
+                      tabs: _customizePages.map((_CustomizePage _page) {
+                        return new Tab(text: _page.text);
+                      }).toList()),
+                ), 
+                new Container(  
+                  height: MediaQuery.of(context).size.width/3,
+                  child: new TabBarView(
+                    controller: _controller,
+                    children: 
+                  _customizePages.map((_CustomizePage _page) {
+                        //return new Tab(text: _page.text);
+                          switch (_page.pos.toInt()) {
+                            case 0:
+                              
+                              return _karat(_getkarat.length);
+                              break;
+                            case 1:
+                        
+                              return _diamond(_getdiamonQuality.length);
+                              break;
+                            case 2:
+                        
+                              return _colour(_getColour.length);
+                              break;
+                            case 3:
+                        
+                              return _standard(1); 
+                              break;
+                            default:
+                              new Text("data");
+                          }
+                      
+                      }).toList()),
+                ),
+                new Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: new Text(
+                    "Product Details",
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                new Container(
+                 height: MediaQuery.of(context).size.width/2,
+                 child: new Container(
+                   margin: const EdgeInsets.only(top:5.0,left:65.0,right:65.0,bottom:5.0),
+                      child: new Column( 
+                        children: _productDetails.map((_ProductDetails _a){
+                        return new Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            new Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child:  new Text(_a.category,textAlign: TextAlign.left,
+                             style: TextStyle(fontSize: 18.0),),
+                            ),
+                            new Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: new Text(_a.value,textAlign: TextAlign.right,
+                             style: TextStyle(fontSize: 18.0),),
+                            ),
+                             
+                          ],
+                        ); 
+                        
+                      }).toList(),
+                      )
+                    ),
+                 
+                ),
+                new Container(
+                  decoration: new BoxDecoration(
+                               border: new Border.all(color: Colors.black26)
+                            ),
+                  padding: const EdgeInsets.all(10.0),
+                  margin: const EdgeInsets.only(top:10.0,left:10.0,right:10.0),
+                  child: new Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children : [
+                    new Column( 
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          new Padding(
+                            padding: const EdgeInsets.only(left:10.0,bottom:5.0),
+                            child : new Text("2 customer reviews",
+                              style: TextStyle(fontSize: 22.0,fontWeight: FontWeight.w500),
+                              textAlign: TextAlign.left,),
+                          ),
+                              new Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: new Row(
+                                
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: <Widget>[
+                                new Row(children: <Widget>[
+                                new Icon(Icons.star),
+                                new Icon(Icons.star),
+                                new Icon(Icons.star), 
+                                new Icon(Icons.star),
+                                new Icon(Icons.star_half),
+                                ],),
+                                
+                                new Text("4.5 out of 5 Stars",style: TextStyle(fontSize: 18.0)),
+                                ],
+                            ),
+                              )
+                        
+                    ],
+                  ),
+                      new Icon(Icons.keyboard_arrow_right),                
+                  ]),
+                ),
+                new Container(
+                  margin: const EdgeInsets.only(top:15.0,left:15.0,right:25.0),
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      new Text("Top Reviews",
+                        style: TextStyle(fontSize: 22.0,fontWeight: FontWeight.w500),
+                        textAlign: TextAlign.left),
+                      new Column(
+                        children: _reviews.map((_ReviewPage _review){
+                                return new Row(
+                                  
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [new Container(
+                                  //decoration: new BoxDecoration(border: new Border.all(color: Colors.black26)),
+                                  padding: const EdgeInsets.all(10.0),
+                                  margin: const EdgeInsets.only(top:5.0,left:10.0,right:10.0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                    new Row(
+                                      children: <Widget>[
+                                        new Icon(Icons.person),
+                                        new Padding(
+                                        padding: const EdgeInsets.only(left:10.0),
+                                        child: new Text(_review.name,
+                                          style: TextStyle(fontSize: 16.0,fontWeight: FontWeight.w400),
+                                          textAlign: TextAlign.left),
+                                        ),
+                                      ],
+                                    ),
+                                    new Row(
+                                      children: <Widget>[
+                                        new Icon(Icons.star),
+                                        new Icon(Icons.star),
+                                        new Icon(Icons.star), 
+                                        new Icon(Icons.star),
+                                        new Icon(Icons.star_border),
+                                      ],
+                                    ),
+                                    new Expanded(
+                                      flex: 0 ,
+                                      child: new Text(_review.text,
+                                      style: TextStyle(fontSize: 16.0,fontWeight: FontWeight.w400),
+                                      maxLines: null,),
+                                    ),
+                                    
+                                      
+                                      
+                                    
+                                    ])),
+                                    new Icon(Icons.keyboard_arrow_right)
+                                    ]);
+                              }).toList(),
+                            ),
+                            
+                          ],
+                        )
+                      
+                ),
+
+
               ],
             ),
           ),
-          new SliverToBoxAdapter(
-              child: new Column(
-                mainAxisSize: MainAxisSize.max, 
-            children: <Widget>[
-              new Container(
-                decoration:
-                    new BoxDecoration(color: Colors.white10),
-                child: new TabBar(
-                    labelColor: Colors.black87,
-                    indicatorWeight: 4.0,
-                    indicatorColor: Colors.black,
-                    controller: _controller,
-                    isScrollable: true,
-                    labelStyle: TextStyle(fontSize: 16.0),
-                    tabs: _customizePages.map((_CustomizePage _page) {
-                      return new Tab(text: _page.text);
-                    }).toList()),
-              ),
-              new Container(
-                height: MediaQuery.of(context).size.width/2.5,
-                child: new TabBarView(
-                    controller: _controller,
-                    children: _customizePages.map((_CustomizePage _page) {
-                      //return new Tab(text: _page.text);
-                      return new GridView.builder(
-                        gridDelegate:
-                            new SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 5,
-                                childAspectRatio: 0.65,
-                            ),
-                        itemBuilder: (BuildContext context, int index) {
-                          return new Container(
-                              padding: const EdgeInsets.all(10.0),
-                              child: new GestureDetector(
-                                  onTap: () {
-                                    print("Colour Tap, $index  -> "+_getColour(index));
-                                  },
-                              child: new Column(
-                                children: [
-                                  new Image.network("https://personalproject1.000webhostapp.com/images/colour_$index.png",
-                                  fit: BoxFit.contain,),
-                                  new Padding(
-                                    padding: const EdgeInsets.only(top: 5.0), 
-                                    child: new Text(_getColour(index),textAlign: TextAlign.center),
-                                  ),
-                                ]
-                              )));
-                        },
-                        itemCount: 4,
-                      );
-                    }).toList()),
-              )
-            ],
-          )),
+        
+
+
           new SliverGrid(
             gridDelegate: new SliverGridDelegateWithMaxCrossAxisExtent(
               maxCrossAxisExtent: MediaQuery.of(context).size.width / 2,
